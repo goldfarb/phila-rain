9eb97229f89d038d68fee3ba75408ecc8add178a
Alice Goldfarb <a_f_goldfarb@yahoo.com>
2022-06-11 15:36:40 -0400

    Update raincheck.py

--- raincheck.py
+++ raincheck.py
@@ -24,15 +24,5 @@ t = str(f'{s.sum():.2f}')
 sms = "Happy birthday! It has rained " + t + " inches in the past three days."
 
 test_val = os.environ['TEST_VAL']
-"""
-account_sid = os.environ('TWILIO_ACCOUNT_SID')
-auth_token = os.environ('TWILIO_AUTH_TOKEN')
-client = Client(account_sid, auth_token)
 
-message = client.messages \
-    .create(
-         body=sms,
-         from_=os.environ('FROM_NUM'),
-         to=os.environ('TO_NUM')
-     )
-"""
+print(test_val)
