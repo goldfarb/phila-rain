ce3b60ef918cfecfda3d1a3acecdc34bd208e31d
goldfarb <a_f_goldfarb@yahoo.com>
2022-06-11 15:39:33 -0400

    [] not ()

--- raincheck.py
+++ raincheck.py
@@ -23,6 +23,13 @@ s = df['6hr']
 t = str(f'{s.sum():.2f}')
 sms = "Happy birthday! It has rained " + t + " inches in the past three days."
 
-test_val = os.environ['TEST_VAL']
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+client = Client(account_sid, auth_token)
 
-print(test_val)
+message = client.messages \
+    .create(
+         body=sms,
+         from_=os.environ['FROM_NUM'],
+         to=os.environ['TO_NUM']
+     )
